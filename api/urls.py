c2573699a980b417e64832ca461dd3273109a1b0
kazukichi <zexyl_x@icloud.com>
2020-09-19 01:26:44 +0900

    api.urls作成

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,14 @@
 from rest_framework import routers
 from django.urls import path
 from django.conf.urls import include
-
+from .views import VideoViewSet, CreateUserView
 
 router = routers.DefaultRouter()
+# ModelViewSetを使用した場合はrouter.registerを使用する
+router.register("videos", VideoViewSet)
 
 urlpatterns = [
+    # CreateUserViewはCreateAPIViewという汎用Viewを使っているのでas_view()のてurlpatternsに記載
+    path("create/", CreateUserView.as_view(), name="create"),
     path("", include(router.urls)),
 ]
